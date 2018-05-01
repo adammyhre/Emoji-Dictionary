5b2b543f50d603ca8b13210e13d6f01d8b9cb930
Adam Myhre <adam@undergalaxie.com>
2018-05-01 02:26:04 -0700

    First code file

---
+++ ViewController.swift
@@ -0,0 +1,22 @@
+//
+// ViewController.swift
+// Emoji Dictionary
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableVewDelegate {
+
+	@IBOutlet weak var mainTableView: UITableView!
+
+	override func viewDidLoad () {
+		super.viewDidLoad()
+
+	}
+
+	override func didReceiveMemoryWarning () {
+		super.didReceiveMemoryWarning ()
+
+	}
+
+}
