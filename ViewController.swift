ad02bf2e5258a7c802a2c8ab27c1592a93d048ca
Adam Myhre <adam@undergalaxie.com>
2018-05-01 02:44:44 -0700

    Going to bed...

--- ViewController.swift
+++ ViewController.swift
@@ -9,9 +9,24 @@ class ViewController: UIViewController, UITableViewDataSource, UITableVewDelegat
 
 	@IBOutlet weak var mainTableView: UITableView!
 
+	var emojis = ["☣", "♪", "♫", "♞", "♟", "♠"]
+
 	override func viewDidLoad () {
 		super.viewDidLoad()
 
+		mainTable.dataSource = self
+		mainTable.delegate = self
+
+	}
+
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return emojis.count;
+	}
+
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = UITableViewCell()
+		cell.textLabel?.text = "☣♪♫♞♟♠"
+		return cell
 	}
 
 	override func didReceiveMemoryWarning () {
